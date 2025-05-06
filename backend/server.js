730cf243974078794992ea8256056ae368e33a23
nguyentam0312 <locnam0312@gmail.com>
2025-05-07 04:24:13 +0700

    done

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(cors({
+    origin: 'https://cartnguyentam.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 app.use(express.json());
 const path = require('path');
 
